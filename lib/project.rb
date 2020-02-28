5d22dbdb4f51407ec6238b05f28ce56e2d03dd07
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-28 05:26:25 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -13,7 +13,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    backer.back_project(self) 
   end 
   
 end 
